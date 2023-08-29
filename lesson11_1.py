b5077837a2a30c15f84aa4fe8923ebcc7f9f07b8
Alan Lin <wnedy55@gmail.com>
2023-08-29 21:23:16 +0800

    202030829

--- lesson11_1.py
+++ lesson11_1.py
@@ -5,15 +5,19 @@ def get_score() -> list:
         score.append(random.randint(50, 100))
     return score
 
+def get_names(nums:int) -> list:
+    with open('names.txt',encoding='utf-8',newline='') as file:
+        names_str = file.read()
+        all_names_list = names_str.split(sep="\n")
+        names_list = random.choices(all_names_list,k=nums) #取出一定數量的姓名
+    return names_list
+
 nums_int = int(input("請輸入學生數:"))
-#取得30個姓名
-with open('names.txt',encoding='utf-8',newline='') as file:
-    names_str = file.read()
-    all_names_list = names_str.split(sep="\n")
-    names_list = random.choices(all_names_list,k=nums_int) #取出一定數量的姓名
-print(names_list)
+#取得nums_int個姓名
+names_list = get_names(nums_int)
 students = []
-for _ in range(nums_int):
+for i in range(nums_int):
     scores = get_score()
-    students.append(scores)
-students
+    new_list = [names_list[i]] + scores
+    students.append(new_list)
+print(students)
